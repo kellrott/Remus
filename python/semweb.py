ee665480562200cbff3f3dca9a7101df2c98d9f3
Kyle <kellrott@viking.(none)>
2011-01-25 16:35:35 -0800

    Refinements to get new pipeline working. Introduces multiple outputs for emits.

--- python/semweb.py
+++ python/semweb.py
@@ -32,22 +32,23 @@ def splitter(f):
 	semweb_functions[ f.__module__ ] = f
 	return f
 
-global out_handle
+global out_handle_map
 
-def emit(key, val):
-	global out_handle
-	out_handle.write( "{\"%s\" : %s}\n" % (key, json.dumps(val)) )
+def emit(key, val, output=None):
+	global out_handle_map
+	out_handle_map[ output ].write( "{\"%s\" : %s}\n" % (key, json.dumps(val)) )
 
-
-def setoutput( handle ):
-	global out_handle
-	out_handle = handle
+def setoutput( outmap ):
+	global out_handle_map
+	out_handle_map = outmap
 
 def getFunction(name):
-	global semweb_functions
+	global out_handle_map
 	return semweb_functions[name]
 
 def init():
 	global semweb_functions
+	global out_handle_map
+	out_handle_map = {}
 	semweb_functions = {}
 	out_handle = sys.stdout
