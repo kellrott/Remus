2eebfc4138e0b4d078e5f95d1683b0a1062e43d3
Kyle Ellrott <kellrott@tungsten.local>
2011-07-18 11:10:12 -0700

    Adding file missing from last checkin

---
+++ src/main/java/org/remus/serverNodes/PipelineInstanceQueryView.java
@@ -0,0 +1,101 @@
+package org.remus.serverNodes;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.mpstore.Serializer;
+import org.remus.RemusInstance;
+import org.remus.RemusPipeline;
+import org.remus.langs.JSInterface;
+import org.remus.mapred.MapCallback;
+import org.remus.work.RemusApplet;
+
+public class PipelineInstanceQueryView implements BaseNode {
+
+	RemusPipeline pipeline;
+	RemusInstance inst;
+	public PipelineInstanceQueryView(RemusPipeline pipeline, RemusInstance inst) {
+		this.pipeline = pipeline;
+		this.inst = inst;
+	}
+
+	@Override
+	public BaseNode getChild(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void doGet(String name, Map params, String workerID,
+			Serializer serial, OutputStream os) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doPut(String name, String workerID, Serializer serial,
+			InputStream is, OutputStream os) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doSubmit(String name, String workerID, final Serializer serial,
+			final InputStream is, final OutputStream os) throws FileNotFoundException {
+
+		RemusApplet applet = pipeline.getApplet(name);
+		if ( applet == null ) {
+			throw new FileNotFoundException();
+		}
+
+		try { 
+			char [] buffer = new char[1024];
+			int len;
+			StringBuilder sb = new StringBuilder();
+			Reader in = new InputStreamReader(is);
+			do {
+				len = in.read(buffer);
+				if ( len > 0 ) {
+					sb.append(buffer, 0, len);
+				}
+			} while ( len >= 0 );
+
+			AppletInstanceView appletView = new AppletInstanceView(applet, inst);
+
+			JSInterface js = new JSInterface();
+			js.init(null);
+			js.initMapper(sb.toString());
+			js.map( appletView, new MapCallback() {				
+				@Override
+				public void emit(String key, Object val) {
+					Map out = new HashMap();
+					out.put(key, val);		
+					try {
+						os.write(serial.dumps(out).getBytes());
+						os.write("\n".getBytes());
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+					);
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void doDelete(String name, Map params, String workerID)
+			throws FileNotFoundException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
